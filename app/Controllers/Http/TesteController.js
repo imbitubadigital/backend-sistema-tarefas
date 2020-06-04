c3660516607aeabe58b4746520ac07a4901b7ee9
Tony Fernandes <imbitubadigital@gmail.com>
2020-06-04 19:11:43 -0300

    refatorando Rota teste

--- app/Controllers/Http/TesteController.js
+++ app/Controllers/Http/TesteController.js
@@ -1,11 +1,11 @@
 'use strict'
 const Task = use('App/Models/Task')
-class TaskController {
-  async get () {
+class TesteController {
+  async index () {
     const task = await Task.all()
 
     return { task, teste: 'Show' }
   }
 }
 
-module.exports = TaskController
+module.exports = TesteController
--- start/routes.js
+++ start/routes.js
@@ -3,7 +3,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.post('teste', 'TesteController.index')
+Route.get('teste', 'TesteController.index')
 Route.post('session', 'SessionController.store')
 Route.post('users', 'ProfileController.store').validator('User/UserCreate')
 
